225ffb176a250ef9da9375e3798b51fb86b0d744
yann6182 <yannickngaleu6@gmail.com>
2023-12-30 19:25:08 +0100

    Refactor routing configuration and remove unnecessary code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,63 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { TabsPage } from './tabs/tabs.page';
+import { DashboardPage } from './dashboard/dashboard.page';
+import { AddOperationPage } from './add-operation/add-operation.page';
 
 const routes: Routes = [
+  
   {
-    path: 'dashboard',
-    loadChildren: () => import('./dashboard/dashboard.module').then( m => m.DashboardPageModule)
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'dashboard',
+        children: [
+          {
+            path: '',
+            component: DashboardPage,
+          },
+        ],
+      },
+      {
+        path: 'add-operation',
+        children: [
+          {
+            path: '',
+            component: AddOperationPage,
+          },
+        ],
+      },
+      
+     
+
+      {
+        path: '',
+        redirectTo: '/tabs/dashboard',
+        pathMatch: 'full',
+      },
+    ],
+  },
+
+  {
+    path: '',
+    redirectTo: '/tabs/dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'tabs/dashboard',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
-    redirectTo: 'dashboard',
+    redirectTo: 'tabs/dashboard',
     pathMatch: 'full'
   },
+  {
+    path: 'tabs/add-operation',
+    loadChildren: () => import('./add-operation/add-operation.module').then( m => m.AddOperationPageModule)
+  },
+ 
   {
     path: 'add-operation',
     loadChildren: () => import('./add-operation/add-operation.module').then( m => m.AddOperationPageModule)
@@ -19,6 +66,11 @@ const routes: Routes = [
     path: 'dashboard',
     loadChildren: () => import('./dashboard/dashboard.module').then( m => m.DashboardPageModule)
   },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
+  }
+  
   
 ];
 
---
+++ src/app/tabs/tabs-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabsPage,
+    children: [
+      {
+        path: 'dashboard',
+        loadChildren: () => import('../dashboard/dashboard.module').then( m => m.DashboardPageModule)
+      },
+     
+      {
+        path: 'add-operation',
+        loadChildren: () => import('../add-operation/add-operation.module').then( m => m.AddOperationPageModule)
+      },
+      
+      
+      {
+        path: '',
+        redirectTo: '/tabs/dashboard',
+        pathMatch: 'full'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabsPageRoutingModule {}
